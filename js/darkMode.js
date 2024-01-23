796e57b5dbef72f4d64f4ded653bd31128655c13
TiPe-30 <121294443+TiPe-30@users.noreply.github.com>
2024-01-23 08:32:06 +0100

    Update darkMode.js

--- js/darkMode.js
+++ js/darkMode.js
@@ -3,10 +3,11 @@ let bouton = document.querySelector("button");
 let scroller = document.querySelector("label");
 document.querySelector("header").style.transition = "300ms";
 const elementEcriture = [document.querySelector("header").querySelectorAll("a"),
- document.querySelectorAll("p"), document.querySelectorAll("h2"),
-  document.querySelectorAll("h1"), document.querySelectorAll("li")];
-
+document.querySelectorAll("p"), document.querySelectorAll("h2"),
+document.querySelectorAll("h1"), document.querySelectorAll("li"), document.querySelector("footer").querySelectorAll("a")];
+const backGroundFooter = document.querySelector("footer").querySelectorAll("div");
 const backGround = document.querySelectorAll("aside");
+
 function functionDarkMode() {
     if (White) {
         /* Partie en mode black */
@@ -22,6 +23,13 @@ function functionDarkMode() {
         } for (let i = 0; i < backGround.length; i++) {
             backGround[i].style.background = "black";
         }
+        /* PARTIE FOOTER */
+        for(let i = 0; i < backGroundFooter.length;i++){
+            if(backGroundFooter[i].getAttribute('class') == "ligne1" || backGroundFooter[i].getAttribute('class') == "ligne2"){
+                backGroundFooter[i].style.backgroundColor = "#1c1c1c";
+            }
+            document.querySelector("footer").querySelector("img").src = "../img/logo-hardis-footer.png";
+        }
     } else {
         /* Partie en mode white */
         White = true;
@@ -36,5 +44,13 @@ function functionDarkMode() {
         } for (let i = 0; i < backGround.length; i++) {
             backGround[i].style.background = "white";
         }
+        /* PARTIE FOOTER */
+        for(let i = 0; i < backGroundFooter.length;i++){
+            if(backGroundFooter[i].getAttribute('class') == "ligne1" || backGroundFooter[i].getAttribute('class') == "ligne2"){
+                backGroundFooter[i].style.backgroundColor = "white";
+            }
+        }
+        document.querySelector("footer").querySelector("img").src = "../img/logo-hardis-group(2).png";
+
     }
 }
